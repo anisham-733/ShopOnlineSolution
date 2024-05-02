ea73a0aed4ebbfd17d122078c194fcf61e5b76b4
Anisha Mahajan <Anisha.Mahajan@insight.com>
2024-05-02 13:33:12 +0530

    GetProducts In Repo layer

---
+++ ShopOnline.API/Repositories/Contracts/ProductRepository.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopOnline.API.Data;
+using ShopOnline.API.Entities;
+
+namespace ShopOnline.API.Repositories.Contracts
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ShopOnlineDbContext _context;
+        public ProductRepository(ShopOnlineDbContext dbContext)
+        {
+            _context = dbContext;
+            
+        }
+        public async Task<IEnumerable<ProductCategory>> GetCategories()
+        {
+            return await _context.ProductCategories.ToListAsync();
+        }
+
+        public Task<ProductCategory> GetCategory(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetItem(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Product>> GetItems()
+        {
+            return await _context.Products.ToListAsync();
+        }
+    }
+}
